0373ab4bfbb342e325b2d09717569911f43501ed
Jensun Ravichandran <jjensun@gmail.com>
2020-11-18 00:49:38 +0100

    Customize dwm

--- config.h
+++ config.h
@@ -5,23 +5,24 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
+static unsigned int borderpx  = 2;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static unsigned int gappih    = 3;        /* horiz inner gap between windows */
+static unsigned int gappiv    = 3;        /* vert inner gap between windows */
+static unsigned int gappoh    = 3;        /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 3;        /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#222222";
+static int topbar             = 0;        /* 0 means bottom bar */
+static char *fonts[]          = { "UbuntuMonoNerdFont:size=14", "JoyPixels:pixelsize=13:antialias=true:autohint=true"  };
+
+static char normbgcolor[]           = "#292b2e";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#f8f8f8";
+static char selbgcolor[]            = "#444155";  // or "#5d4d7a"
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
