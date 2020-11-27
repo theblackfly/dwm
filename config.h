ae4b65e4cdbaaaacfc71e2508c939915eee7bac0
Jensun Ravichandran <jjensun@gmail.com>
2020-11-27 14:31:26 +0100

    Update documentation

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 0;        /* 0 means bottom bar */
-static char *fonts[]          = { "UbuntuMonoNerdFont:size=14", "JoyPixels:pixelsize=13:antialias=true:autohint=true"  };
+static char *fonts[]          = { "UbuntuMonoNerdFont:size=14", "JoyPixels:pixelsize=17:antialias=true:autohint=true"  };
 
 static char normbgcolor[]           = "#292b2e";
 static char normbordercolor[]       = "#444444";
