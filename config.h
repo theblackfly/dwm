b0c080eb4cb11e2c8048639bf2378955f4b781d9
Jensun Ravichandran <jjensun@gmail.com>
2021-07-12 18:32:03 +0200

    Add systray patch
    See https://dwm.suckless.org/patches/systray/ for more info

--- config.h
+++ config.h
@@ -17,6 +17,12 @@ static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 0;        /* 0 means bottom bar */
 static char *fonts[]          = { "UbuntuMonoNerdFont:size=14", "JoyPixels:pixelsize=17:antialias=true:autohint=true"  };
 
+static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 0; /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2; /* systray spacing */
+static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1; /* 0 means no systray */
+
 static char normbgcolor[]           = "#292b2e";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
